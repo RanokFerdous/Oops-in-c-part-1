e0c2e27d354f462dbbaff8125b9f06760ca1a14d
RanokFerdous <164327286+RanokFerdous@users.noreply.github.com>
2024-12-17 11:37:51 +0600

    Add files via upload

---
+++ c.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+int main()
+{
+
+   vector<int>v={ 1,2,3};
+
+   do{
+        for ( auto u:v)cout<<<u<<" ";
+    cout<<endl;
+
+   }while( next_permutation( v.begin(), v.end()));
+
+}
